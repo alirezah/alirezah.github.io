b4cbca43506b0a7a2221bbae2076c9a0bf0f9fbc
alirezah <alirezah@vt.edu>
2020-12-30 19:26:13 -0800

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -2,15 +2,34 @@
  * Proof-of-Concept Angular Controller
  */
 
-var myApp = angular.module('actionsApp', ['ui'])
-    .controller('actionsController', function($scope, $http) {
-        const actionsUrl = 'https://cors-anywhere.herokuapp.com/https://alirezah.github.io/json/sample_actions.json'
+var myApp = angular.module("actionsApp", ["ui"])
+    .directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit('ngRepeatFinished');
+                    });
+                }
+            }
+        }
+    })
+    .controller("actionsController", function($scope, $http) {
         $http
-            .get(actionsUrl)
+            .get("../data/actions.json")
             .success(function(data) {
-                $scope.actions = data.actions
+                $scope.actions = data.actions;
             })
             .error(function(data) {
-                console.log('Error while processing course data!')
-            })
+                console.log("Error while processing course data!");
+            });
+        
+        $scope.$on("ngRepeatFinished", function(ngRepeatFinishedEvent) {
+            $("p").expander({
+                slicePoint: 200,
+                expandEffect: "fadeIn",
+                collapseEffect: "fadeOut"
+            });
+        });
     });
